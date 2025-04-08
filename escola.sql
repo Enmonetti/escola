cd1089e1e5de501ec29e7ed8c58e2b7a1cbe4d57
Enmonetti <erick.monetti2@gmail.com>
2025-04-07 22:47:10 -0300

    first commit

---
+++ escola.sql
@@ -0,0 +1,30 @@
+
+CREATE DATABASE escola;
+USE escola;
+CREATE TABLE curso (
+    codcurso CHAR(2) PRIMARY KEY,
+    nome VARCHAR(50),
+    mensalidade DECIMAL(10,2)
+);
+CREATE TABLE aluno (
+    ra CHAR(9) PRIMARY KEY,
+    rg CHAR(9),
+    nome CHAR(30),
+    codcurso CHAR(2), 
+    FOREIGN KEY (codcurso) REFERENCES curso(codcurso)
+);
+CREATE TABLE disciplina (
+    coddisc VARCHAR(5) PRIMARY KEY,
+    nomedis CHAR(30),	
+    nrocreditos INT,
+    codcurso CHAR(2),
+    FOREIGN KEY (codcurso) REFERENCES curso(codcurso)
+);
+CREATE TABLE boletim (
+    ra CHAR(9),
+    coddisc VARCHAR(5),
+    nota DECIMAL(4,2),
+    PRIMARY KEY (ra, coddisc),
+    FOREIGN KEY (ra) REFERENCES aluno(ra),
+    FOREIGN KEY (coddisc) REFERENCES disciplina(coddisc)
+);
